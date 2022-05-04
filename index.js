3651b47c37f3f0dd3c6f26e306816572521c0749
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-05-04 11:47:26 +0530

    establish connection to mongodb cloud

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-// mongodb+srv://hardikbachhan:hardikbachhan@cluster0.mntie.mongodb.net/blog?retryWrites=true&w=majority
-
 const express = require("express");
 const app = express();
 const port = 3000;
+
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
