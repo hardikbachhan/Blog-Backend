ffe00cb2cf786c542e72e3facf4ec648da2348d6
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-05-10 11:21:05 +0530

    add route to create a new user and code to read json sent from client

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
+
+const authRouter = require("./routes/auth");
 
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 dotenv.config();
+
+app.use(express.json());    // to read json data sent from client.
 
 mongoose
     .connect(process.env.MONGO_URL, {
@@ -18,6 +22,8 @@ mongoose
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.use("/api/auth", authRouter)
 
 app.listen(port, () => {
     console.log(`Blog app listening on port ${port}`);
---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const authRouter = express.Router();
+const User = require("../models/User");
+
+//REGISTER
+
+authRouter.post("/register", async (req, res) => {
+    try {
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            profilePicture: req.body.profilePicture,
+        });
+
+        const user = await newUser.save();
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+//LOGIN
+
+module.exports = authRouter;
