a8cfda1938f35d22d3fb7573c169e5841e269363
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-05-10 11:35:18 +0530

    add bcrypt package to hash password before saving in db

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,20 @@ const express = require("express");
 const authRouter = express.Router();
 const User = require("../models/User");
 
+const bcrypt = require('bcrypt');
+
 //REGISTER
 
 authRouter.post("/register", async (req, res) => {
     try {
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
             profilePicture: req.body.profilePicture,
         });
 
